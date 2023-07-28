abba6bbe3f7b3f4711f60c1915c3364a69253161
smart <mail@>
2023-07-28 16:51:58 +0200

    Works with with small and long phrases

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import pyperclip
@@ -19,28 +21,34 @@ import time
 # -sending chain as argument
 # - con alt shift to back cursor to last position
 
+# take text, add to list, until text is empty, take second to last
+
+
 TARGET_URL = 'https://speechnotes.co/dictate/'
 XPATH_LIBRARY = {
     'record_button': '//div[@id="start_button"]',
-    'text_area': '//div[@id="output_box"]/textarea'
+    'text_area': '//div[@id="output_box"]/textarea',
+    'text_mirror': '//div[@id="mirror_container"]//div',
 }
 
 
 def print_help():
-    print(f'''This is a python selenium refresher: 
-          d.get("https://en.wikipedia.org/wiki/Main_Page") 
-          //tag[contains(@attribute, 'substring')]
-          //div[contains(text(), 'Google')]"# actions = ActionChains(d)
+    print(f'''
+    
+            This is a python selenium refresher: 
+            d.get("https://en.wikipedia.org/wiki/Main_Page") 
+            //tag[contains(@attribute, 'substring')]
+            //div[contains(text(), 'Google')]"# actions = ActionChains(d)
             actions.send_keys(Keys.ENTER).perform()
             d.switch_to.active_element
             with open('f2.txt', 'w', encoding="utf-8") as f: f.write(d.page_source)
-          
-          How to use: 
-          --f for find_element(By.XPATH, '//button')
-          ie: --f target_xpath -> e = find_element(By.XPATH, '//target_xpath')
-          --fs for find_elementS(By.XPATH, '//button'
-          driver.find_elements(By.XPATH, '//button')
-          
+            
+            How to use: 
+            --f for find_element(By.XPATH, '//button')
+            ie: --f target_xpath -> e = find_element(By.XPATH, '//target_xpath')
+            --fs for find_elementS(By.XPATH, '//button'
+            driver.find_elements(By.XPATH, '//button')
+  
             
           ''')
 
@@ -48,7 +56,8 @@ def print_help():
 def create_driver():
     import undetected_chromedriver as uc
 
-    driver = uc.Chrome(headless=False, use_subprocess=False)
+    driver = uc.Chrome(headless=True, use_subprocess=False)
+    driver.maximize_window()
 
     return driver
 
@@ -118,106 +127,55 @@ def grant_permissions():
 
 def record_and_paste():
 
-    # click in start recording button
-    # measure time of no change in text area
-    #    after 2 secs of no change in text area, copy and paste
-    #       copy text area (selenium Ctrl+c)
-    #       paste text area (pyautogui Ctrl+v)
-    #       delete text area (selenium Ctrl+a, delete)
-    # stop recording
-
     # start recording
     d.find_element(By.XPATH, XPATH_LIBRARY['record_button']).click()
 
-    # wait to think about to what to say
-    # sleep(2)
+    text = get_text()
 
-    # if no change in text area for x secs, copy and paste
-    while not is_text_stable(interval=1.8):
-        # copy_text_to_clipboard(d)
-        paste_text()  # pyautogui in user clipboard
-        delete_text()  # selenium in site's text area
-        break
+    paste_text(text)
 
     # stop recording
     d.find_element(By.XPATH, XPATH_LIBRARY['record_button']).click()
 
 
-def is_text_stable(interval=1.5):
+def get_text():
+    # tm = text_mirror
+    tm = d.find_element(By.XPATH, XPATH_LIBRARY['text_mirror'])
+    wait_until_started_to_change(tm)
+
+    text_chain = []
 
     while True:
-        previous_length = get_text_len(d)
-        time.sleep(interval)
-        current_length = get_text_len(d)
-        print(current_length ,' ' , previous_length)
 
-        if current_length == previous_length:
-            return False
+        sleep(0.1)
+        tm = d.find_element(By.XPATH, XPATH_LIBRARY['text_mirror'])
+        text_chain.append(tm.text)
+        # print('str: ' + str + '\n' + 'tm.text: ' + tm.text)
+        # print(text_chain[-1])
 
-
-def get_text_len(d):
-    # return clipboard length
-    copy_text_to_clipboard(d)
-    return len(pyperclip.paste())
-
-
-def copy_text_to_clipboard(d):
-    # click in text area
-    # control + a
-    # control + c
-
-    # select_all_text(d)  # control + a
-    # actions = ActionChains(d)
-    # actions.key_down(Keys.CONTROL).send_keys('c').perform()
-    # actions.key_up(Keys.CONTROL).perform()
-
-    ta = d.find_element(By.XPATH, XPATH_LIBRARY['text_area'])
-    actions = ActionChains(d)
-    actions.move_to_element(ta).key_down(Keys.CONTROL).send_keys('a').send_keys('c').perform()
-    actions.key_up(Keys.CONTROL).perform()
-
-    # ta = d.find_element(By.XPATH, XPATH_LIBRARY['text_area'])
-    # ta.send_keys(Keys.CONTROL, 'c')
+        if tm.text == '':  # when input to recording ends, it sets to ''
+            # the last normally is '' -> [-2], unless only one word is said [-1]
+            try:
+                return text_chain[-2]
+            except:
+                return text_chain[-1]
 
 
-    print('copied ' + pyperclip.paste())
+def wait_until_started_to_change(element):
+    while True:
+        sleep(0.1)
+        if element.text != '':
+            return
 
 
-def paste_text():
+def paste_text(text):
+    pyperclip.copy(text)
     pyautogui.hotkey('ctrl', 'v')
-    print('pasted' + pyperclip.paste())
-    sleep(2)
 
-
-def delete_text():
-    select_all_text(d)
-    actions = ActionChains(d)
-    actions.send_keys(Keys.DELETE).perform()
-    print('deleted')
-
-
-def select_all_text(d):
-    ta = d.find_element(By.XPATH, XPATH_LIBRARY['text_area'])
-    ta.send_keys(Keys.CONTROL, 'a')
-
-    # actions = ActionChains(d)
-    # actions.key_down(Keys.CONTROL).send_keys('a').perform()
-    # actions.key_up(Keys.CONTROL).release(ta).perform()
-    # actions.send_keys_to_element(ta, actions.key_down(Keys.CONTROL), 'a').perform()
 
 def close_driver(d):
     d.close()
     d.quit()
-
-# d.find_element(By.XPATH, XPATH_LIBRARY['record_button']).click()
-# //div[@id="output_box"]
-# d.find_element(By.XPATH, '//div[@id="output_box"]').click()
-# d.find_element(By.XPATH, '//div[@id="output_box"]/textarea')
-# actions.move_to_element(ta).key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).key_up(Keys.CONTROL).perform()
-# actions.move_to_element(ta).key_down(Keys.CONTROL).send_keys('c').perform()
-# actions.send_keys(Keys.CONTROL, 'a').perform()
-# d.find_element(By.XPATH,
-
 
 
 def run():
@@ -231,16 +189,17 @@ def run():
 
     keyboard.add_hotkey('ctrl+shift+ñ', record_and_paste)  #args=('Hello from shortcut!',)
 
+    # only for testing
     # execution_wheel(d)
 
     while True:
-        keyboard.wait('esc')
-        # i = input("Enter additional data (or 'exit' to stop): ")
-        # if i == 'q':
-        #     break
-        # if i == '':
-        #     pyperclip.copy(t + i)
-        #     pyperclip.paste()
+
+        keyboard.wait('esc')  # trigger shortcut
+
+        i = input("Enter additional data (or 'exit' to stop): ")
+        if i == 'q':
+            print('exiting ... ')
+            break
 
 
 if __name__ == '__main__':

40ea81a24529cf102dac173ea439f39051f8c7e4
smart <mail@>
2023-10-01 10:45:00 +0200

    funcs added

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from time import sleep
 import pyperclip
 import keyboard
 import pyautogui
-# from selenium.webdriver import Keys
 # from selenium.webdriver.common.alert import Alert
 # from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -44,6 +43,16 @@ def create_driver():
     return driver
 
 
+def create_normal_driver():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    # from webdriver_manager.firefox import GeckoDriverManager
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  #, options=chrome_options)
+    return driver
+
+
 def grant_permissions(d):
     d.execute_cdp_cmd(
         "Browser.grantPermissions",
@@ -55,7 +64,7 @@ def grant_permissions(d):
     )
 
 
-def record_and_paste(d):
+def execute_iteration(d):
 
     # start recording
     d.find_element(By.XPATH, XPATH_LIBRARY['record_button']).click()
@@ -71,24 +80,24 @@ def record_and_paste(d):
 
 
 def print_grammar_correction(d, text):
+    switch_tabs(d)
 
-    # open grammar page
-    d.get(TARGET_URLS['grammar_url'])
+    # send text
+    # include xpath in path library Json
+    d.find_element(By.XPATH, '//d-textarea[contains(@class, "lmt__source_textarea")]').send_keys(text)
 
-    # paste text
-    d.find_element(By.XPATH, '//textarea').send_keys(text)
+    # DEBT: create dynamic wait until the icon of 'working' starts and dissappears
+    sleep(1.5)
 
-    # wait for grammar correction
-    sleep(1)
+    # copy text
+    d.find_element(By.XPATH, '//d-textarea[contains(@class, "lmt__textarea lmt__target_textarea")]').click()
 
-    # get corrected text
-    corrected_text = d.find_element(By.XPATH, '//textarea').text
+    # print in CLI
 
-    # paste corrected text
-    paste_text(corrected_text)
+    # clean input field for next grammar correction
 
-    # go back to dictation page
-    d.get(TARGET_URLS['dictation_url'])
+
+    switch_tabs(d)
 
 
 def get_text(d):
@@ -102,7 +111,7 @@ def get_text(d):
         tm = d.find_element(By.XPATH, XPATH_LIBRARY['text_mirror'])
         text_chain.append(tm.text)
 
-        if tm.text == '':  # when input recording ends, it sets to ''
+        if tm.text == '':  # when input recording ends, it sets itself to ''
             # the last normally is '' -> [-2], unless only one word is said [-1]
             try:
                 return text_chain[-2]
@@ -138,23 +147,50 @@ def close_driver(d):
     d.quit()
 
 
-def open_tabs(d):
-    d.execute_script("window.open('');")
-    d.switch_to.window(d.window_handles[1])
-    d.get(TARGET_URLS['grammar_url'])
-    d.switch_to.window(d.window_handles[0])
+def open_second_tab(d, url):
+    d.execute_script(f'''window.open("{url}","_blank");''')
+
+
+# def open_tabs(d):
+#     d.execute_script("window.open('');")
+#     d.switch_to.window(d.window_handles[1])
+#     d.get(TARGET_URLS['grammar_url'])
+#     d.switch_to.window(d.window_handles[0])
+
+def switch_tabs(d):
+    current_tab = d.current_window_handle
+
+    # check all the tabs, switch to the one that differs from the current tab
+    for tab in d.window_handles:
+        if tab != current_tab:
+            d.switch_to.window(tab)
+
+
+def switch_to_american_english(d):
+    switch_tabs(d)
+
+    # click button to switch language
+    d.find_element(By.XPATH, '//button[@id="headlessui-listbox-button-10"]').click()
+
+    sleep(0.5)
+
+    # click in american
+    d.find_element(By.XPATH, '//span[contains(text(), "American")]').click()
 
 
 def run():
-    d = create_driver()
-
-    open_tabs(d)  # open dictation and grammar tabs
+    # d = create_driver()
+    d = create_normal_driver()
 
     d.get(TARGET_URLS['dictation_url'])
 
     grant_permissions(d)  # microphone, geo location, camera
 
-    keyboard.add_hotkey('ctrl+shift+ñ', lambda: record_and_paste(d))
+    open_second_tab(d, TARGET_URLS['grammar_url']) # open grammar tab
+
+    switch_to_american_english(d)
+
+    keyboard.add_hotkey('ctrl+shift+ñ', lambda: execute_iteration(d))
 
     while True:
         try:
